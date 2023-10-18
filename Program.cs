c32fbefede520ffa4e6ed0fc63cd8df6c772d34b
LAB-49 <LAB-49@LAB-49-PC02>
2023-10-18 10:40:43 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BernalPizza
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            const double SLICE_AREA = 14.13;
+
+            double radius;
+            int numberOfSlices;
+
+            Console.WriteLine("Enter the Radius of the Pizza");
+            radius = double.Parse(Console.ReadLine());
+
+            numberOfSlices = (int)((PI * radius * radius) / SLICE_AREA);
+
+            Console.WriteLine($"{numberOfSlices} IServiceProvider the total slices of the pizza");
+
+
+
+        }
+    }
+}
